f1b98396e11288e211e28dc82e5720925477f62a
ardis.roznieks <ardis.roznieks@gmail.com>
2021-02-20 10:50:50 +0200

    8uzd

---
+++ 8uzdevums.py
@@ -0,0 +1,6 @@
+print("ievadi veselu skaitli:")
+x = int(input())
+if x>100 and x<1000 :
+    print("skaitlis ir intervala starp 100 un 1000")
+else:
+    print("skaitlis nav intervala starp 100 un 1000")
