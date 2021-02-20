56f2c7a64c5d22894c1bdd0688b7479737afe4b6
ardis.roznieks <ardis.roznieks@gmail.com>
2021-02-20 10:59:07 +0200

    9uzd

---
+++ 9uzdevums.py
@@ -0,0 +1,7 @@
+print("ievadi savu savru(kg):")
+svars = int(input())
+print ("ievadi savu augumu(cm):")
+augums = int(input())
+
+bmi = svars/((augums/100)**2)
+print("jusu kermena masas indekss ir:",bmi)
